080a70c530074ef08ba484d7edde51e3563f020e
10148120 <sh827kim@gmail.com>
2023-03-03 23:29:55 +0900

    store 생성, 영화 리스트, 영화 아이템, 더보기 버튼, 검색창 컴포넌트 추가

--- src/core/core.js
+++ src/core/core.js
@@ -60,7 +60,9 @@ export class Store {
                 get: () => state[key],
                 set: (val) => {
                     state[key] = val
-                    this.observers[key].forEach(observer => observer(val))
+                    if(Array.isArray(this.observers[key])) {
+                        this.observers[key].forEach(observer => observer(val))
+                    }
                 }
             })
         }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,13 @@
-import { createRouter } from "../core/core"
+import { createRouter } from '../core/core'
 import Home from './Home'
+import Movie from "./Movie";
 export default createRouter([
     {
         path: '#/',
         component: Home
+    },
+    {
+        path: '#/movie',
+        component: Movie
     }
 ])
