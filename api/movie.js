3501cd5ae1e8013aee3d6322d7215baa3bf27e03
Spark Kim <sh827kim@gmail.com>
2023-03-04 19:40:29 +0900

    Update movie.js

--- api/movie.js
+++ api/movie.js
@@ -5,7 +5,7 @@ const { API_KEY } = process.env
 export default async function handler(request, response) {
   const { title, page, id } = JSON.parse(request.body)
   const url = id
-    ? `https://www.omdbapi.com/?apikey=${API_KEY}&s=i=${id}&plot=full`
+    ? `https://www.omdbapi.com/?apikey=${API_KEY}&i=${id}&plot=full`
     : `https://www.omdbapi.com/?apikey=${API_KEY}&s=${title}&page=${page}`
 
   const res = await fetch(url)
